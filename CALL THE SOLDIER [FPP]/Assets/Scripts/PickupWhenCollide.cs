dbe3ac0a73293d687809fa0765f93be509f9cc3c
rifatahammed <reardj007@gmail.com>
2022-02-24 03:55:42 +0600

    Detect collusion with pickup layer

---
+++ CALL THE SOLDIER [FPP]/Assets/Scripts/PickupWhenCollide.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupWhenCollide : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask PickupMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.gameObject.layer == LayerMask.NameToLayer("Pickup"))
+        {
+            Debug.Log("Touched a rail");
+        }
+    }
+}
